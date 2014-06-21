da1f782b10f4f5c9e805ed3eecde7b6f39b68716
skewart <scott.ewart@gmail.com>
2014-06-21 10:20:58 -0400

    Tweaked timing in jobs to not cause the whole thing to timeout.

--- jobs/four_square_trending.py
+++ jobs/four_square_trending.py
@@ -16,7 +16,7 @@ redirect = os.environ['FOURSQUARE_REDIRECT']
 # Set some query parameters
 ll = '22.2670,114.1880' 
 #ll = '40.7127, -74.0059' #NY
-total_time = 270
+total_time = 30 
 
 def get_checkins(ll):
     # Construct the client object
--- jobs/twitter.py
+++ jobs/twitter.py
@@ -23,9 +23,9 @@ def get_tweets( latlong=None ):
 def get_lots_of_tweets( latlong ):
 	""" Does pretty much what its long name suggests. """
 	all_tweets = {}
-	total_time = 270
+	total_time = 30
 	remaining_seconds = total_time
-	interval = 30
+	interval = 30 
 	while remaining_seconds > 0:
 		added = 0
 		new_tweets = get_tweets( latlong )
