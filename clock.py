6cbe680473ee4db1a9a4070867f1016d207400e4
skewart <scott.ewart@gmail.com>
2014-06-21 16:26:56 -0400

    Something else

--- clock.py
+++ clock.py
@@ -17,9 +17,8 @@ q = Queue(connection=conn)
 # intelligently - but here's a little hand-rolled cache of just the methods I want to call.
 JOBS = {}
 
-@sched.interval_schedule(minutes=5)
-def timed_job():
-	""" Runs the jobs defined by code in the jobs directory. """
+def setup_jobs():
+	""" Does the initial gathering of the jobs. """
 	for mod_lo, name, ispkg in pkgutil.iter_modules(["./jobs"]):
 		if name not in JOBS:
 			fullname = "jobs." + name
@@ -28,15 +27,20 @@ def timed_job():
 			if 'run' in dir( this_job ):
 				JOBS[ name ] = getattr( this_job, 'run' )
 				print "Added %s to the JOBS cache" % name
-			else:
-				JOBS[ name ] = None
-				continue
-		if JOBS[ name ] is None:
-			continue	
-        print "Adding %s to the jobs queue" % name
-        q.enqueue( JOBS[name] )
+			#else:
+			#	JOBS[ name ] = None
+			#	continue
 
 
+@sched.interval_schedule(minutes=5)
+def timed_job():
+	""" Runs the jobs defined by code in the jobs directory. """
+		for name, job in JOBS.iteritems():
+	        print "Adding %s to the jobs queue" % name
+	        q.enqueue( job )
+
+
+setup_jobs()
 sched.start()
 
 while True:
