7d04c2768be1ab56a0a7fed17f87ed5a367d289f
cesandoval <blackbox@sghkcore.c.engaged-fact-636.internal>
2014-07-17 13:45:05 +0000

    some short paths

--- shortestpaths.py
+++ shortestpaths.py
@@ -22,17 +22,30 @@ def getShortestPaths():
     queryString = """
     MATCH (a:FourSqrVenues_explore)<-->(b:FourSqrUsers) 
     MATCH  p = allShortestPaths((a)<-->(b))
-    RETURN p, a, b
+    RETURN p
     limit 100
     """
+    """
+    START n = node:points_hk('bbox:[ 22.3, 115.0, 22.271, 114.18 ]') 
+    WHERE (n:FourSquareVenues_explore) 
+    RETURN n
+    """
+    """START n=node:points_hk('withinDistance:[ 22.35, 114.2, 15.0 ]')
+    WHERE (n:FourSqrVenues_explore) return n"""
+    
+    queryString = '''MATCH (u:FourSqrVenues_explore) RETURN u.lat, u.lon''' 
     
     batch.append_cypher(queryString)
     results = batch.submit() 
+    for i in results[0]:
+        lat, lon = i[0], i[1]
+        batch.append_cypher("START n=node:points_hk('withinDistance:[ %s, %s, 15.0 ]') WHERE (n:FourSqrVenues_explore) return n" %(lat, lon))
+    results = batch.submit() 
     print results
-    
+    '''
     for i in results:
         print i
-        print
+        print'''
           
 if __name__=="__main__":
     #DB.clear()
